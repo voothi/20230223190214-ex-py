0633db5360f1f96acfd4aa65f57c3a967c891d4b
Denis Novikov <voothi@gmail.com>
2023-02-23 21:06:18 +0100

    Add code 7_2

--- 7_2.py
+++ 7_2.py
@@ -1,8 +1,19 @@
 # 7.2
 
-# [ ] 
-# [ ] 
+# [ ]
+# [ ]
 
 
+def get_order_amount(price_of_portion_of_ice_cream: int,
+                     price_of_bottle_of_soda: int,
+                     number_of_portion_of_ice_cream: int,
+                     number_of_bottle_of_soda: int):
+    return \
+        price_of_portion_of_ice_cream * number_of_portion_of_ice_cream + \
+        price_of_bottle_of_soda * number_of_bottle_of_soda
+
+
+sum_order_stefania = get_order_amount(1, 4, 3, 2)
+sum_order_michailo = get_order_amount(1, 4, 2, 3)
 
 
