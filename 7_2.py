3b0c8bc8e78bd19ed3284cabfbb9049b74e17566
Denis Novikov <voothi@gmail.com>
2023-02-23 21:31:20 +0100

    Add testcases

--- 7_2.py
+++ 7_2.py
@@ -3,7 +3,6 @@
 # [ ]
 # [ ]
 
-
 def get_order_amount(price_of_portion_of_ice_cream: int,
                      price_of_bottle_of_soda: int,
                      number_of_portion_of_ice_cream: int,
@@ -13,7 +12,32 @@ def get_order_amount(price_of_portion_of_ice_cream: int,
         price_of_bottle_of_soda * number_of_bottle_of_soda
 
 
+def get_analyse_summary(sum_order_stefania, sum_order_michailo):
+    if sum_order_stefania < sum_order_michailo:
+        print("Стефания потратила - "
+              + str(sum_order_stefania)
+              + ", что меньше, чем Михайло - "
+              + str(sum_order_michailo))
+    elif sum_order_stefania > sum_order_michailo:
+        print("Стефания потратила - "
+              + str(sum_order_stefania)
+              + ", что больше, чем Михайло - "
+              + str(sum_order_michailo))
+    else:
+        print("Стефания потратила - "
+              + str(sum_order_stefania)
+              + ", что столько же, сколько Михайло - "
+              + str(sum_order_michailo))
+
+# Тестирование
 sum_order_stefania = get_order_amount(1, 4, 3, 2)
 sum_order_michailo = get_order_amount(1, 4, 2, 3)
+get_analyse_summary(sum_order_stefania, sum_order_michailo)
 
+sum_order_stefania = get_order_amount(1, 4, 3, 2)
+sum_order_michailo = get_order_amount(1, 4, 3, 2)
+get_analyse_summary(sum_order_stefania, sum_order_michailo)
 
+sum_order_stefania = get_order_amount(1, 4, 2, 3)
+sum_order_michailo = get_order_amount(1, 4, 3, 2)
+get_analyse_summary(sum_order_stefania, sum_order_michailo)
